408c85dd8e14622bcb045bf398cfda4b839c4ced
Chloe Kim <chloeminkyung@gmail.com>
2018-10-21 11:06:09 +0800

    commit

--- scenarios/temperature.py
+++ scenarios/temperature.py
@@ -5,16 +5,15 @@ def handle_express_cold(user):
     requests.post("http://localhost:5000/api/v1/temperature/create_temperature_feeling", json=[{"feeling": "cold"}])
 
     res = requests.get(url='http://localhost:5000/api/v1/temperature/temperature_concensus_cold')
-    res = json.load(res.text)
-    print(res)
+    res = res.text
 
-    return "We will try to weaken the air conditioning soon! There are " + len(res) + " number of people who feel cold as well."
+    return "We will try to weaken the air conditioning soon! There are " + str(len(res)) + " number of people who feel cold as well."
 
 
 def handle_express_hot(user):
     requests.post("http://localhost:5000/api/v1/temperature/create_temperature_feeling", json=[{"feeling": "hot"}])
 
     res = requests.get(url='http://localhost:5000/api/v1/temperature/temperature_concensus_hot')
-    res = json.load(res.text)
+    res = res.text
 
-    return "We will try to strengthen the air conditioning soon! There are " + len(res) + " number of people who feel hot as well."
+    return "We will try to strengthen the air conditioning soon! There are " + str(len(res)) + " number of people who feel hot as well."
